70b9b5bb8f655465f08c92c79039a7e53ff71717
Finn Lawrence <finn.lawrence@gmail.com>
2017-05-31 23:52:09 +1200

    Printing to debug

--- _site/js/script.js
+++ _site/js/script.js
@@ -126,6 +126,7 @@ const contactForm = {
         var request = new XMLHttpRequest();
 
         request.addEventListener("load", function () {
+            console.log(request);
             if (request.status === 302 || request.status === 0) { // CloudCannon redirects on success
                 contactForm.setFormStatus("complete");
             }
